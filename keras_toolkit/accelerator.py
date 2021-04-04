f0f4d69f8da1b9807820629933121419ca68d112
xhlulu <xhlperso@gmail.com>
2021-04-03 20:17:42 -0400

    Add a limit-gpu-memory function

--- keras_toolkit/accelerator.py
+++ keras_toolkit/accelerator.py
@@ -23,3 +23,26 @@ def auto_select(verbose: bool = True) -> "tf.distribute.Strategy":
         print(f"Running on {strategy.num_replicas_in_sync} replicas")
 
     return strategy
+
+
+def limit_gpu_memory(limit: str = None, verbose: bool = True, device: int = 0):
+    """
+    *Limits the amount of GPU memory (VRAM) used by TensorFlow.*
+
+    {{params}}
+    {{limit}} The limit value in megabytes.
+    {{verbose}} Whether to display the restriction message.
+    {{device}} Which GPU we want to limit the memory, if there are more than one.
+
+    **Warning: This function uses experimental features from TensorFlow!**
+    """
+    gpus = tf.config.experimental.list_physical_devices("GPU")
+    if gpus:
+        try:
+            tf.config.experimental.set_virtual_device_configuration(
+                gpus[device],
+                [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=limit)],
+            )
+        except RuntimeError as e:
+            print(e)
+        print(f"We will restrict TensorFlow to max {limit/1024:.2f}GB GPU RAM")
